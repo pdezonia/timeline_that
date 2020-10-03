9331fb1ac4610427c4ee2951af26f9e1af7555f4
Philip H. deZonia <pdezonia@gmail.com>
2020-10-03 18:27:11 -0400

    Make main file and planning document

---
+++ timeline_that_main.py
@@ -0,0 +1,9 @@
+"""
+|<--------------------------------------------------------------------------->|
+|<------------------------------------------------------------------->|
+
+timeline_that
+by Philip deZonia
+Made to help with planning out stories by adding and moving around events
+Started on 10/3/20 (Fall) in Philadelphia
+"""
